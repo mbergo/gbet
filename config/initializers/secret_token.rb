877fdd2170c30092ed6710ef983e60ff5cad522e
Marcus Bergo <marcusbergo@gmail.com>
2015-07-08 15:54:43 -0300

    fail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamebet::Application.config.secret_key_base = 'b69a89f544496f4c03673c909e0fa005eb72baa8709276bef571afe28a79fc5353b1dd1b5358419df1a02c809ddd9e9c64199854794b867e87b8872deab4838a'
